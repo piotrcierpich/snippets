00d9cd15d3782c872a3012ebc9ac729a6af93eb4
Piotrek <piotr.cierpich@gmail.com>
2013-10-29 15:58:17 +0100

    textbox validation

---
+++ wpf/TextBox validation/WpfApplication3/MainWindow.xaml.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Windows;
+
+namespace WpfApplication3
+{
+  /// <summary>
+  /// Interaction logic for MainWindow.xaml
+  /// </summary>
+  public partial class MainWindow : Window
+  {
+    public MainWindow()
+    {
+      InitializeComponent();
+      DataContext = new ViewModel();
+    }
+  }
+
+  class ViewModel : INotifyPropertyChanged, IDataErrorInfo
+  {
+    public event PropertyChangedEventHandler PropertyChanged;
+
+    public ViewModel()
+    {
+      TextChangedReads = new ObservableCollection<string>();
+    }
+
+    private string text = string.Empty;
+    public string Text
+    {
+      get
+      {
+        TextChangedReads.Add("read: " + text);
+        return text;
+      }
+      set
+      {
+        text = value;
+        OnPropertyChanged("Text");
+      }
+    }
+
+    public ObservableCollection<string> TextChangedReads { get; set; }
+
+    protected virtual void OnPropertyChanged(string propertyName = null)
+    {
+      PropertyChangedEventHandler handler = PropertyChanged;
+      if (handler != null) handler(this, new PropertyChangedEventArgs(propertyName));
+    }
+
+    public string this[string columnName]
+    {
+      get
+      {
+        return columnName == "Text" && text.Length < 5
+                ? "Age must not be less than 0 or greater than 150."
+                : null;
+      }
+    }
+
+    public string Error { get { return null; } }
+  }
+}
